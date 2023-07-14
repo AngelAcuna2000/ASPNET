cef534953533473198dc48026211d372076d392c
Angel Acuna <AngelAcuna2000@gmail.com>
2023-07-14 11:01:47 -0400

    Part 3 Complete

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -22,5 +23,23 @@ namespace Testing.Controllers
             var product = _repo.GetProduct(id);
             return View(product);
         }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = _repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            _repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
     }
 }
